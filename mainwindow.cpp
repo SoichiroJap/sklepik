fb74ca68dd26e45b33261cca00a8a586fdce944b
SoichiroJap <turczak123@gmail.com>
2025-05-27 19:49:44 +0200

    dopracowanie ekranu startowego

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,33 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->pbShoppingList, &QPushButton::clicked,
+            this, &MainWindow::handleShoppingListClicked);
+
+    connect(ui->pbStore, &QPushButton::clicked,
+            this, &MainWindow::handleStoreClicked);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::handleShoppingListClicked()
+{
+    qDebug() << "Kliknięto: Lista zakupów";
+    // otworzyć liste
+}
+
+void MainWindow::handleStoreClicked()
+{
+    qDebug() << "Kliknięto: Sklep";
+    //otworzyc sklep
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,9 +4,7 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -14,10 +12,15 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void handleShoppingListClicked();
+    void handleStoreClicked();
 
 private:
     Ui::MainWindow *ui;
 };
+
 #endif // MAINWINDOW_H
